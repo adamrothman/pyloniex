a4ae8f644d66604cd3d2173e2b7547e45e2b9db4
Adam Rothman <rothman.adam@gmail.com>
2017-09-05 20:28:08 -0700

    Distinguish between request errors and server errors

--- pyloniex/api/__init__.py
+++ pyloniex/api/__init__.py
@@ -2,19 +2,24 @@
 from abc import ABCMeta
 from abc import abstractmethod
 from time import sleep
+from logging import getLogger
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Optional
 from typing import Union
 
 from requests import Request
 from requests import Session
 
-from pyloniex.errors import PoloniexAPIError
+from pyloniex.errors import PoloniexRequestError
+from pyloniex.errors import PoloniexServerError
 from pyloniex.utils import RateLimiter
 
 
 REQUESTS_PER_SECOND = 6
+
+logger = getLogger(__name__)
 
 
 class PoloniexBaseAPI(metaclass=ABCMeta):
@@ -37,7 +42,7 @@ class PoloniexBaseAPI(metaclass=ABCMeta):
         self,
         *args,
         **kwargs,
-    ) -> Union[Dict[str, Any], List[Dict[str, Any]]]:
+    ) -> Optional[Union[Dict[str, Any], List[Dict[str, Any]]]]:
         cls_name = type(self).__name__
         while self._rate_limiter.check(cls_name) is False:
             sleep(1 / self._requests_per_second)
@@ -46,8 +51,24 @@ class PoloniexBaseAPI(metaclass=ABCMeta):
         prepared = self._session.prepare_request(request)
 
         response = self._session.send(prepared)
-        data = response.json()
-        if 'error' in data:
-            raise PoloniexAPIError(response.status_code, data['error'])
+        try:
+            data = response.json()
+        except ValueError:
+            logger.exception('Error decoding response JSON')
+            data = None
 
-        return data
+        status = response.status_code
+        if status >= 200 and status < 300:
+            return data
+
+        error = None
+        if isinstance(data, dict) and 'error' in data:
+            error = data['error']
+
+        if status >= 400 and status < 500:
+            raise PoloniexRequestError(status, error)
+        elif status >= 500:
+            raise PoloniexServerError(status, error)
+        else:
+            # We never expect to get a 3xx; if we do, just return None
+            return None
--- pyloniex/errors.py
+++ pyloniex/errors.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
+from typing import Optional
 
 
 class PoloniexAPIError(Exception):
 
-    def __init__(self, status_code: int, message: str) -> None:
+    def __init__(
+        self,
+        status_code: int,
+        message: Optional[str] = None,
+    ) -> None:
         self.status_code = status_code
         self.message = message
 
     def __str__(self):
-        return f'[{self.status_code}] {self.message}'
+        s = f'[{self.status_code}]'
+        if self.message is not None:
+            s += f' {self.message}'
+        return s
+
+
+class PoloniexRequestError(PoloniexAPIError):
+    pass
+
+
+class PoloniexServerError(PoloniexAPIError):
+    pass
